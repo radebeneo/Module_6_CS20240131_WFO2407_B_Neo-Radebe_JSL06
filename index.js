5c8d9122789cda6c6d9cc348f425dea990312e60
umkhulubhungane <admin@Apple-MacBook-Pro-Retina-13-Early-2015.local>
2024-09-13 12:53:33 +0200

    Get the menu container element from the HTML

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const menu = {
 // Function to display menu items by category
 function displayMenuItems(menu) {
     // Get the menu container element from the HTML
+    const menuContainer = document.getElementById('menu');
 
     // Loop through each category and its items in the menu object
 
