c04dd74f80e7e900e962c8b0f99d1de64126245a
umkhulubhungane <admin@Apple-MacBook-Pro-Retina-13-Early-2015.local>
2024-09-15 16:18:32 +0200

    Get the order items list and the order total element from the HTML

--- index.js
+++ index.js
@@ -45,8 +45,13 @@ function displayMenuItems(menu) {
 
 // Callback function for adding an item to the order
 function addToOrder(itemName) {
+    let order = [];
+    let totalPrice = 0;
     // Get the order items list and the order total element from the HTML
-
+    return function(itemName) {
+        const orderItemsList = document.getElementById('order-items');
+        const orderTotalElement = document.getElementById('order-total');
+        
     // Create a list item for the order
     const orderItem = document.createElement('li');
     // Set the text content of the list item to the item name
